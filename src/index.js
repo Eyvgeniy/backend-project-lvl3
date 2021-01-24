3a456a1632976947de05d60b527f8700359b9a33
Eyvgeniy <antonm33@yandex.ru>
2021-01-24 20:21:09 +0200

    Enable concurrent mode in Listr

--- src/index.js
+++ src/index.js
@@ -46,7 +46,7 @@ const savePage = (dir, url) => {
     })
     .then(() => {
       const promiseLinks = new Listr(
-        allLinks.map((link) => ({ title: link, task: () => buildPromise(link) })),
+        allLinks.map((link) => ({ title: link, task: () => buildPromise(link) })), {concurrent: true}
       );
       return promiseLinks.run();
     })
